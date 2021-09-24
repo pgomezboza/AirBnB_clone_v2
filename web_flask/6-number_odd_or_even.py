f16cd6be6428892b7e174af0f2c015bd161a785f
pgomezboza <pgomezboza@gmail.com>
2021-09-24 10:46:25 -0500

    add task 6

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+ script that starts a Flask web application
+ must be listening on 0.0.0.0, port 5000
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def main():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def text_c(text):
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def text_py(text='is cool'):
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def num(n):
+    if isinstance(n, int):
+        return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def html_num(n):
+    if isinstance(n, int):
+        return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def html_num_oddeven(n):
+    odd_even = 'even' if n % 2 == 0 else 'odd'
+    return render_template('6-number_odd_or_even.html', n=n, odd_even=odd_even)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
